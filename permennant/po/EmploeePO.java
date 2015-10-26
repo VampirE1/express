c45276c13ab7e3ce96cfa0107069b925fcdf7f7a
yp <1013799318@qq.com>
2015-10-26 19:38:24 +0800

    PO类
    增加PO类里的东西

--- permennant/po/EmploeePO.java
+++ permennant/po/EmploeePO.java
@@ -13,12 +13,57 @@ public class EmploeePO implements Serializable {
 	private ArrayList<EmploeePO> emploee;
 	
    public  EmploeePO(String ID){
-	   this.ID=ID;
-	   this.name=name;
-	   this.age=age;
-	   this.institution=institution;
-	   this.salary=salary;
-	    emploee = new ArrayList<EmploeePO>();
+	   this.setID(ID);
+	   
+	    setEmploee(new ArrayList<EmploeePO>());
 	}
+
+public String getID() {
+	return ID;
+}
+
+public void setID(String iD) {
+	ID = iD;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+public String getInstitution() {
+	return institution;
+}
+
+public void setInstitution(String institution) {
+	this.institution = institution;
+}
+
+public double getSalary() {
+	return salary;
+}
+
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+
+public ArrayList<EmploeePO> getEmploee() {
+	return emploee;
+}
+
+public void setEmploee(ArrayList<EmploeePO> emploee) {
+	this.emploee = emploee;
+}
    
 }
